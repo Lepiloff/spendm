9923b64e051d2e167236be786f0d553acf2b49b8
e.lepilov <e.lepilov@softteco.com>
2020-04-13 10:43:51 +0300

    SM-55 - [FIX] - fix vendor profile update,

---
+++ service/module-parent_category_intersection.py
@@ -0,0 +1,25 @@
+module_to_category = {"Sourcing": "", "Supplier Management": "", "Spend Analytics": "", "Contract Management": "", "e-Procurement": "", "Invoice-to-Pay": "", "Strategic Procurement": "", "Technologies": "" }
+
+MODULES_NAME = (
+    ("Sourcing", "Sourcing"),
+    ("Supplier Management", "Supplier Management"),
+    ("Spend Analytics", "Spend Analytics"),
+    ("Contract Management", "Contract Management"),
+    ("e-Procurement", "e-Procurement"),
+    ("Invoice-to-Pay", "Invoice-to-Pay"),
+    ("Strategic Procurement", "Strategic Procurement"),
+    ("Technologies", "Technologies"),
+    ("Procure-to-Pay", "Procure-to-Pay"),
+    ("Source-to-Pay", "Source-to-Pay"),
+)
+
+PC = (
+    ("Common S2P", "Common S2P"),
+    ("Common Sourcing - SXM", "Common Sourcing - SXM"),
+    ("Services", "Services"),
+    ("Sourcing", "Sourcing"),
+    ("SXM", "SXM"),
+    ("Spend Analytics", "Spend Analytics"),
+    ("CLM", "CLM"),
+    ("eProcurement", "eProcurement"),
+    ("I2P", "I2P"),)
