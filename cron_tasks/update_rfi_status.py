b4738d1c11821c7fc39229b931ceb29b2d835a2f
e.lepilov <e.lepilov@softteco.com>
2020-06-03 12:55:52 +0300

    SM-129 - [DEV] - add change status to Accepted then Issued to rfi triggered, fix SM-173

--- cron_tasks/update_rfi_status.py
+++ cron_tasks/update_rfi_status.py
@@ -1,5 +1,5 @@
 import datetime
-from apps.vendors.models import Rfis
+from apps.vendors.models import Rfis, RfiParticipationStatus
 
 
 def update_rfi_status():
@@ -8,12 +8,15 @@ def update_rfi_status():
     try:
         current_date = datetime.datetime.today()
         opened_rfies = Rfis.objects.filter(active=True, open_datetime__date__lt=current_date,
-                                           issue_datetime__date__gt=current_date).exclude(
-            rfi_status="Opened")
+                                           issue_datetime__date__gt=current_date).exclude(rfi_status="Opened")
         issued_rfies = Rfis.objects.filter(active=True, issue_datetime__date__lte=current_date).exclude(rfi_status="Issued")
-
-        opened_rfies.update(rfi_status="Opened")
-        issued_rfies.update(rfi_status="Issued")
+        if opened_rfies:
+            opened_rfies.update(rfi_status="Opened")
+        if issued_rfies:
+            issued_rfies.update(rfi_status="Issued")
+            rps = RfiParticipationStatus.objects.filter(rfi=issued_rfies.first())
+            if rps:
+                rps.update(status='Accepted')
 
     except Exception as e:
         print(e)
